d0327acbc1fcff825c823b26e8a0ebdc74285236
Roxanne Guo <roxane.guo@gmail.com>
2012-02-17 13:00:06 -0500

    having trouble with self :(

--- q4.py
+++ q4.py
@@ -1,6 +1,23 @@
 #! /usr/bin/env python
 
-def divisible(num, divisor):
+class q4(self):
+    def loop_divisible(self, num):
+        # check if the number is divisible by 1 to 20
+        divisor = 1
+        while divisor <= 20:
+            if num%divisor == 0:
+                divisor += 1
+            else:
+                return False
+        return True
+
+    def find_lowest_divident(self, number):
+        print self.loop_divisible(number)
+        while(not self.loop_divisible(number)):
+            test_number += 2
+        return test_number
+
+def recursion_divisible(num, divisor):
     # check if the number is divisible by 1 to 20
     if divisor <= 20:
         # if divisible
